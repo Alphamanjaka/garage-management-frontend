b697eb64770f63dd4d7e8c0117fa977612e66555
dregh1 <rakotonirinacharlesandrea@gmail.com>
2025-03-13 02:46:51 +0300

    service-list

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { ServiceListComponent } from "../service-list/service-list.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ServiceListComponent, ServiceListComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+ 
+
 export class HomeComponent {
+  searchQuery : any;
 
 }
---
+++ src/app/services/service.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+  private apiUrl = environment.apiUrl+"/services";
+  constructor(private http: HttpClient) { }
+
+  getServices(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+  addService(Service: any): Observable<any> {
+    return this.http.post(this.apiUrl, Service);
+  }
+  updateService(id: string, Service: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, Service);
+  }
+  deleteService(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
