bddceb1cf6fb8e56d1250f974d49efc3ee9506e7
alphamanjaka <alphamanjaka@gmail.com>
2025-03-13 10:48:11 +0300

    front
    banniere-footer
    en general page d'acceuil

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -5,16 +5,17 @@ import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
-  selector: 'app-offer-list',
-  imports: [MatCardModule, MatProgressBarModule, MatChipsModule],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.css',
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-
-  templateUrl: './offer-list.component.html',
-  styleUrl: './offer-list.component.css'
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OfferListComponent {
+export class BannerComponent {
   longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
   Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
   usually kept as a companion animal or for showing.`;
+
+  constructor() { }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
+import { BannerComponent } from "../banner/banner.component";
+import { OfferListComponent } from "../offer-list/offer-list.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent, BannerComponent, OfferListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
