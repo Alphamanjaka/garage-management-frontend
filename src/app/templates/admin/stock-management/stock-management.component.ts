29b860db065ca5ecdd0481cab48074f82c3ae802
dregh1 <rakotonirinacharlesandrea@gmail.com>
2025-03-22 11:09:09 +0300

    add new piece

--- src/app/templates/admin/stock-management/stock-management.component.ts
+++ src/app/templates/admin/stock-management/stock-management.component.ts
@@ -1,16 +1,18 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-stock-management',
-  imports: [FormsModule, NgFor],
+  imports: [FormsModule, NgFor, NgIf],
   templateUrl: './stock-management.component.html',
   styleUrl: './stock-management.component.scss'
 })
 export class StockManagementComponent {
   searchTerm: string = '';
 
+  newPiece = { name: '', price: 0, ref: '' };
+  showModal: boolean = false;
 
   pieces = [
     { name: 'Vis M8', price: 2.5, ref: 'VIS-M8' },
@@ -25,5 +27,23 @@ export class StockManagementComponent {
       piece.ref.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
       piece.price.toString().includes(this.searchTerm)
     );
+  }
+
+  addPiece() {
+    if (this.newPiece.name && this.newPiece.price > 0 && this.newPiece.ref) {
+      this.pieces.push({ ...this.newPiece });
+      this.newPiece = { name: '', price: 0, ref: '' };
+    } else {
+      alert('Veuillez remplir tous les champs avec des valeurs valides.');
+    }
+  }
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+    this.newPiece = { name: '', price: 0, ref: '' }; // Réinitialiser le formulaire
   }
 }
