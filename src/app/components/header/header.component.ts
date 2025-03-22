9c8f88270031a1e446371fb7a83ec0ff0f87dd7f
dregh1 <rakotonirinacharlesandrea@gmail.com>
2025-03-22 07:03:18 +0300

    user-role

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,34 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { UserRole } from '../../models/UserRole';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   searchQuery: any;
+  userRole : string ="";
+  UserRole = UserRole;
+
+  constructor(){
+    this.userRole = "";
+
+    if (typeof window !== 'undefined' && window.localStorage) {
+      
+      let role = localStorage.getItem("role");
+
+      if (role !== null && role !== "" && role !== undefined) {
+        console.log("ss",role);
+        
+        this.userRole = role;
+      }
+    }
+  }
+
   toggleMore() {
 
   }
---
+++ src/app/models/UserRole.ts
@@ -0,0 +1,5 @@
+export enum UserRole {
+    ADM = 'ADMIN',
+    CLI = 'CLIENT',
+    EMP = 'EMPLOYE', 
+  }
