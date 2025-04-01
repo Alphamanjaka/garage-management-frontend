5476fa9b0c68218cae6d835ce844c7994fac8ba5
alphamanjaka <alphamanjaka@gmail.com>
2025-04-01 20:07:07 +0300

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { FooterComponent } from "../footer/footer.component";
 import { BannerComponent } from "../banner/banner.component";
 import { HeaderComponent } from "../header/header.component";
+import { ServiceService } from '../../services/service.service';
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent, BannerComponent, HeaderComponent],
+  imports: [FooterComponent, BannerComponent, HeaderComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 
@@ -12,6 +15,18 @@ import { HeaderComponent } from "../header/header.component";
 
 
 export class HomeComponent {
-  searchQuery : any;
+  searchQuery: any;
+  servicesList: any;
+
+  constructor(private serviceServices: ServiceService) { }
+
+  ngOnInit() {
+    this.fetchServices();
+  }
+  fetchServices() {
+    this.serviceServices.getServices()
+      .subscribe(data => this.servicesList = data.slice(0,4) );
+
+  }
 
 }
