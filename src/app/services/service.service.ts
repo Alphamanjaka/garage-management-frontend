0ef1c7564b9fdfc7a9d48f68df1a5d8fa44d9f7e
alphamanjaka <alphamanjaka@gmail.com>
2025-04-02 12:02:43 +0300

    Update service.service.ts

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/env.prod';
+import { environment } from '../../environments/env';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
